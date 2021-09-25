0a8099dcc54a368e0be77886e7d61dfd060cbd6e
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-25 20:32:40 +0300

    example of securely storing passwords

--- src/index.js
+++ src/index.js
@@ -13,3 +13,17 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+const bcrypt = require("bcryptjs");
+const myFunction = async () => {
+  const password = "red1234?";
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare("red1234?", hashedPassword);
+  console.log(isMatch);
+};
+
+myFunction();
