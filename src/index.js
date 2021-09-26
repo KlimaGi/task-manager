fbef2b4fc7695f156c5dc22a341576d253202b8c
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-26 13:41:16 +0300

    express middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET requests are disabled");
+//   } else {
+//     next();
+//   }
+// });
+
+app.use((req, res, next) => {
+  res.status(503).send("Site is currently down. Check back soon!");
+});
+
 app.use(express.json()); // parse data to obj
 app.use(userRouter); // register
 app.use(taskRouter);
